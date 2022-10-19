b7c0f5f7fa054bf4ab0bc93c47793c3b8b9e7c31
Rahul-Singh-Dhek <rahuldhek21@gmail.com>
2022-10-19 16:09:26 +0530

    RD

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -15,6 +15,15 @@ const createOrder = async function (req, res) {
         if (!req.body.cartId) {
             return res.status(400).send({ status: false, message: "please provide cartId in request body " })
         }
+        let cancellable;
+        if (req.body.cancellable) {
+            if(typeof req.body.cancellable!=Boolean){
+            return res.status(400).send({ status: false, message: "Cancellable must be in boolean." })
+            }
+            cancellable = req.body.cancellable
+        } else {
+            cancellable = true
+        }
         let cart = await cartModel.findById(req.body.cartId).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 }).lean()
         if (cart.userId.toString() != req.params.userId) {
             return res.status(400).send({ status: false, message: "This cartId does not belong to given userId " })
@@ -27,12 +36,8 @@ const createOrder = async function (req, res) {
             return res.status(400).send({ status: false, message: "Cart does not have any items to make orders." })
         }
         let totalQuantity = 0
-        let cancellable;
-        if (req.body.cancellable) {
-            cancellable = req.body.cancellable
-        } else {
-            cancellable = true
-        }
+
+        
 
         for (let ele of cart.items) {
             totalQuantity = totalQuantity + ele.quantity;
@@ -40,10 +45,11 @@ const createOrder = async function (req, res) {
 
         let order = { ...cart, totalQuantity: totalQuantity, cancellable: cancellable, status: "pending", deletedAt: null, isDeleted: false }
 
+
         let savedOrder = await orderModel.create(order);
         await cartModel.findByIdAndUpdate({ _id: req.body.cartId }, { items: [], totalItems: 0, totalPrice: 0 }, { new: true })
 
-        return res.status(201).send({ status: false, message: "success", data: savedOrder });
+        return res.status(201).send({ status: true, message: "success", data: savedOrder });
 
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message })
